5ff53286a2b6f271c847725547fe2314c9e1632e
Juan Canelon <liscanojac@gmail.com>
2024-11-20 21:26:42 -0300

    feat: interfaces for date-calculator

--- src/interfaces/date-calculator.ts
+++ src/interfaces/date-calculator.ts
@@ -1,6 +1,54 @@
-export interface DateDifference {
-  years: string
-  months: string
-  weeks: string
-  days: string
+export interface TimeDifference {
+  years: {
+    total: number
+    months: number
+    months_days: number
+    weeks: number
+    weeks_days: number
+  }
+  months: {
+    total: number
+    weeks: number
+    weeks_days: number
+    days: number
+  }
+  weeks: {
+    total: number
+    days: number
+  }
+  days: {
+    total: number
+  }
+}
+
+export type DateOptions = {
+  y_m_d: boolean
+  m_d: boolean
+  d: boolean
+  w_d: boolean
+}
+
+export type DateOptionKey = 'y_m_d' | 'm_d' | 'w_d' | 'd'
+
+export type DateMethod = (startDate: Date, endDate: Date) => Date
+
+export type TimeDifferenceMethods = {
+  y_m_d: Array<DateMethod>
+  m_d: Array<DateMethod>
+  d: Array<DateMethod>
+  w_d: Array<DateMethod>
+}
+
+export interface DateDifferenceObject {
+  y_m_d: string
+  m_d: string
+  d: string
+  w_d: string
+}
+
+export interface formatTimeMethods {
+  y_m_d: () => string
+  m_d: () => string
+  d: () => string
+  w_d: () => string
 }
