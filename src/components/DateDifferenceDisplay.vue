f4cea50262d173e2f8f7bc14d12f519c81047c07
Juan Canelon <liscanojac@gmail.com>
2024-11-22 08:04:37 -0300

    feat: DateDifferenceDisplay component

---
+++ src/components/DateDifferenceDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="(difference, differenceKey) in dateDifference" :key="differenceKey">
+    <p v-if="dateOptions[differenceKey]">{{ difference || '\u200B' }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import type { DateDifferenceObject, DateOptions } from '@/interfaces/date-calculator'
+import { defineComponent, type PropType } from 'vue'
+
+export default defineComponent({
+  name: 'DateDifferenceDisplayComponent',
+  props: {
+    dateDifference: {
+      type: Object as PropType<DateDifferenceObject>,
+      required: true,
+    },
+    dateOptions: {
+      type: Object as PropType<DateOptions>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
