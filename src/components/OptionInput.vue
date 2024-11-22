0bdf6d4cf5fb0d18442101d9142d177c1329b666
Juan Canelon <liscanojac@gmail.com>
2024-11-22 08:06:13 -0300

    feat: OptionInput component

---
+++ src/components/OptionInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :name="inputName"
+      :id="inputName"
+      :checked="optionModel"
+      @input="$emit('update:optionModel', ($event.target as HTMLInputElement).checked)"
+    />
+    <label :for="inputName">{{ inputLabel }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'OptionInputComponent',
+  props: {
+    optionModel: {
+      type: Boolean,
+      required: true,
+    },
+    inputLabel: {
+      type: String,
+      required: true,
+    },
+    inputName: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['update:optionModel'],
+})
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
