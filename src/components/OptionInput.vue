f5bac1c7cc979d5c8be07fa7ab9c750ac2e4fec7
Juan Canelon <liscanojac@gmail.com>
2024-11-29 15:29:06 -0300

    feat: label slots

--- src/components/OptionInput.vue
+++ src/components/OptionInput.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="my-1">
+  <div class="my-1 flex items-center">
+    <label for="left-label"><slot name="left-label"></slot></label>
     <Toggle
-      :classes="toggleClasses"
+      :classes="getToggleClasses"
       :model-value="optionModel"
       @update:model-value="$emit('update:optionModel', $event)"
     />
-    <label class="ml-2">{{ inputLabel }}</label>
+    <label for="right-label"><slot name="right-label"></slot></label>
+    <label v-if="inputLabel" class="ml-2">{{ inputLabel }}</label>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, type PropType } from 'vue'
 import Toggle from '@vueform/toggle'
+import type { toggleInputClasses } from '@/interfaces/date-calculator'
 
 export default defineComponent({
   name: 'OptionInputComponent',
@@ -25,18 +28,21 @@ export default defineComponent({
     },
     inputLabel: {
       type: String,
-      required: true,
+      required: false,
+    },
+    toggleClasses: {
+      type: Object as PropType<toggleInputClasses>,
     },
   },
   data() {
     return {
-      toggleClasses: {
+      toggleClassesDefault: {
         container:
           'inline-block rounded-full outline-none focus:ring focus:ring-light-mint focus:ring-opacity-30',
         toggle:
           'flex w-12 h-5 rounded-full relative cursor-pointer transition items-center box-content border-2 text-xs leading-none',
         toggleOn: 'bg-primary-mint border-primary-mint',
-        toggleOff: 'bg-gray-200 border-gray-200 justify-end text-gray-700',
+        toggleOff: 'bg-light-blue border-light-blue justify-end text-gray-700',
         toggleOnDisabled:
           'bg-gray-300 border-gray-300 justify-start text-gray-400 cursor-not-allowed',
         toggleOffDisabled:
@@ -49,6 +55,11 @@ export default defineComponent({
         label: 'text-center w-8 border-box whitespace-nowrap select-none',
       },
     }
+  },
+  computed: {
+    getToggleClasses(): toggleInputClasses {
+      return { ...this.toggleClassesDefault, ...this.toggleClasses }
+    },
   },
   emits: ['update:optionModel'],
 })
