36b207f5f693ccf08a7c02fe686a36057fc34ddd
Juan Canelon <liscanojac@gmail.com>
2024-11-29 15:26:34 -0300

    feat: darkMode prop

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -12,6 +12,7 @@
       :text-input="textInputOptions"
       :placeholder="placeholder"
       :required="required"
+      :dark="darkMode"
       :utc="true"
       auto-apply
       esc-close
@@ -56,6 +57,10 @@ export default defineComponent({
       type: Boolean,
       default: true,
     },
+    darkMode: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
@@ -91,4 +96,33 @@ export default defineComponent({
 
 <style scoped>
 /* Add your styles here */
+.dp__theme_dark {
+  --dp-background-color: var(--dark-blue);
+  --dp-text-color: #fff;
+  --dp-hover-color: #484848;
+  --dp-hover-text-color: #fff;
+  --dp-hover-icon-color: #959595;
+  --dp-primary-color: #005cb2;
+  --dp-primary-disabled-color: #61a8ea;
+  --dp-primary-text-color: #fff;
+  --dp-secondary-color: #a9a9a9;
+  --dp-border-color: #212121;
+  --dp-menu-border-color: #2d2d2d;
+  --dp-border-color-hover: #aaaeb7;
+  --dp-border-color-focus: #aaaeb7;
+  --dp-disabled-color: #737373;
+  --dp-disabled-color-text: #d0d0d0;
+  --dp-scroll-bar-background: #212121;
+  --dp-scroll-bar-color: #484848;
+  --dp-success-color: #00701a;
+  --dp-success-color-disabled: #428f59;
+  --dp-icon-color: #959595;
+  --dp-danger-color: #e53935;
+  --dp-marker-color: #e53935;
+  --dp-tooltip-color: #3e3e3e;
+  --dp-highlight-color: rgb(0 92 178 / 20%);
+  --dp-range-between-dates-background-color: var(--dp-hover-color, #484848);
+  --dp-range-between-dates-text-color: var(--dp-hover-text-color, #fff);
+  --dp-range-between-border-color: var(--dp-hover-color, #fff);
+}
 </style>
