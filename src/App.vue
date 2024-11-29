fc0a6e8d7bc8b6a51120f769417b57eb76b914cb
Juan Canelon <liscanojac@gmail.com>
2024-11-29 15:21:39 -0300

    feat: adding header and  footer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,47 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import DateCalculator from './components/DateCalculator.vue'
+DateCalculator
+import DarkModeInput from './components/DarkModeInput.vue'
+import { Icon } from '@iconify/vue'
+
+const darkMode = ref(false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="flex items-center justify-around py-2 px-4">
+    <img alt="Vue logo" class="h-10 w-10 md:h-12 md:w-12" src="./assets/logo.svg" />
+    <h1 class="text-xl md:text-2xl lg:text-3xl font-semibold">Vue Date Calculator</h1>
+    <DarkModeInput v-model:dark-mode="darkMode" />
   </header>
-
   <main>
-    <TheWelcome />
+    <DateCalculator :dark-mode="darkMode" />
   </main>
+  <footer class="absolute bottom-0 w-full">
+    <div class="grid items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-7 py-4">
+      <div class="md:col-start-2 md:col-span-2 lg:col-start-3 lg:col-span-3">
+        <div class="flex justify-center items-center">
+          <h4 class="flex text-center">
+            Made with <span class="text-xl px-1"><Icon icon="line-md:heart-filled" /></span> and Vue
+          </h4>
+        </div>
+      </div>
+      <div class="md:col-start-4 lg:col-start-7">
+        <div class="flex justify-center items-center text-2xl">
+          <a class="mr-2" href="https://x.com/liscanojac" target="_blank" rel="noopener noreferrer">
+            <Icon icon="line-md:twitter-x" />
+          </a>
+          <a
+            href="https://github.com/liscanojac/vue-date-calculator"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon icon="line-md:github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
