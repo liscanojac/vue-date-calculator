1aecdf8679e45b223658269cc8c9602d7a781f77
Juan Canelon <liscanojac@gmail.com>
2024-12-02 17:30:58 -0300

    test: submodule fucntionality

--- src/components/OptionsPanel.vue
+++ src/components/OptionsPanel.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="text-xl font-medium mx-1">Options</h2>
+    <h2 class="text-xl font-medium mx-1">{{ optionTitle }}</h2>
     <div class="my-2">
       <div class="" v-for="(option, optionKey) in dateOptions" :key="optionKey">
         <OptionInput
@@ -20,6 +20,7 @@
 import type { DateDifferenceObject, DateOptions } from '@/interfaces/date-calculator'
 import { defineComponent, type PropType } from 'vue'
 import OptionInput from './OptionInput.vue'
+import { returnOptionsTitle } from '@/services/date-calculator/src/date-calculator/testSkaffolding'
 
 export default defineComponent({
   name: 'OptionsPanelComponent',
@@ -50,6 +51,9 @@ export default defineComponent({
         !this.dateOptions.w_d &&
         !this.dateOptions.y_m_d
       )
+    },
+    optionTitle() {
+      return returnOptionsTitle()
     },
   },
   methods: {
