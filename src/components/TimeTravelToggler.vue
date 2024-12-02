53fc96296b3bc8356a51493a400f2d6315e52488
Juan Canelon <liscanojac@gmail.com>
2024-12-02 12:06:57 -0300

    feat: timeTravel toggler component

---
+++ src/components/TimeTravelToggler.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <OptionInput
+      :option-model="timeTravel"
+      :input-label="timeTravelLabel"
+      @update:option-model="$emit('update:timeTravel', $event)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import OptionInput from './OptionInput.vue'
+
+export default defineComponent({
+  name: 'TimeTravelTogglerComponent',
+  components: {
+    OptionInput,
+  },
+  props: {
+    timeTravel: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      timeTravelLabel: 'Add to or Subtract from a Date',
+    }
+  },
+  methods: {},
+  emits: ['update:timeTravel'],
+})
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
