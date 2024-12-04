ee3e81b0669335c7d59f7f3dafd2046f95cee2e3
Juan Canelon <liscanojac@gmail.com>
2024-12-04 16:14:19 -0300

    fix: value in prop input

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -38,7 +38,7 @@
           class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="999"
           required
-          v-model="numberValue"
+          :value="numberValue"
           @input="filterNumbers"
         />
         <button
