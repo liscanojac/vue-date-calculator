2d96023f90a84a0ac27e32cbd74a811f436e7e40
Juan Canelon <liscanojac@gmail.com>
2024-12-04 15:51:30 -0300

    feat: dateTravelOptions data update

--- src/components/DateCalculator.vue
+++ src/components/DateCalculator.vue
@@ -17,12 +17,20 @@
           :dark-mode="darkMode"
         />
         <DatePicker
+          v-if="!dateTravel"
           v-model:date-model="endDate"
           :min-date="startDate"
           placeholder="Select End Date"
-          :required="true"
+          :required="!dateTravel"
           :picking-same-date="false"
           :dark-mode="darkMode"
+        />
+        <DateTravelPanel
+          v-if="dateTravel"
+          :date-travel-options="dateTravelOptions"
+          :date-travel-direction="goingFuture"
+          @update:going-future="updateDateTravelDirection"
+          @update:date-travel-options="updateDateTravelOptions"
         />
         <button
           type="submit"
@@ -32,18 +40,28 @@
           Calculate
         </button>
       </form>
-      <OptionsPanel :date-options="dateOptions" @update:date-options="updateDateOptions" />
+      <TimeTravelToggler v-model:time-travel="dateTravel" />
+      <OptionsPanel
+        v-if="!dateTravel"
+        :date-options="dateOptions"
+        @update:date-options="updateDateOptions"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import type { DateOptions } from '@/services/date-calculator/src/interfaces/date-calculator'
+import type {
+  DateOptions,
+  TimeTravelOptionsBase,
+} from '@/services/date-calculator/src/interfaces/date-calculator'
 import { dateCalculator } from '@/services/date-calculator/src/date-calculator/dateDifference'
 import OptionsPanel from './OptionsPanel.vue'
 import DateDifferenceDisplay from './DateDifferenceDisplay.vue'
 import DatePicker from './DatePicker.vue'
+import TimeTravelToggler from './TimeTravelToggler.vue'
+import DateTravelPanel from './DateTravelPanel.vue'
 
 export default defineComponent({
   name: 'DateCalculatorComponent',
@@ -51,6 +69,8 @@ export default defineComponent({
     OptionsPanel,
     DateDifferenceDisplay,
     DatePicker,
+    TimeTravelToggler,
+    DateTravelPanel,
   },
   props: {
     darkMode: {
@@ -76,10 +96,21 @@ export default defineComponent({
         w_d: true,
         d: true,
       },
+      dateTravel: false,
+      goingFuture: true,
+      dateTravelOptions: {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+      },
     }
   },
   computed: {
     btnEnabled(): boolean {
+      return this.dateTravel ? this.dateTravelEnabled : this.bothDatesEnabled
+    },
+    bothDatesEnabled() {
       return (
         !!this.startDate &&
         !!this.endDate &&
@@ -87,6 +118,15 @@ export default defineComponent({
           this.dateOptions.m_d ||
           this.dateOptions.w_d ||
           this.dateOptions.y_m_d)
+      )
+    },
+    dateTravelEnabled() {
+      return (
+        !!this.startDate &&
+        (!!this.dateTravelOptions.years ||
+          !!this.dateTravelOptions.months ||
+          !!this.dateTravelOptions.weeks ||
+          !!this.dateTravelOptions.days)
       )
     },
   },
@@ -111,6 +151,12 @@ export default defineComponent({
       if (!this.dateDisplayEmpty()) {
         this.getDateDifference()
       }
+    },
+    updateDateTravelDirection(value: boolean) {
+      this.goingFuture = value
+    },
+    updateDateTravelOptions(optionKey: keyof TimeTravelOptionsBase, value: boolean) {
+      this.dateTravelOptions = { ...this.dateTravelOptions, [optionKey]: value }
     },
   },
 })
